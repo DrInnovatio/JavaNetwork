35620db23d3cbd14e7c2b251af26035b51c9b58f
DrInnovatio <turing1723@gmail.com>
2021-12-22 11:03:49 +0900

    the second commit and the basic newtwork.

---
+++ src/Ex01_01.java
@@ -0,0 +1,21 @@
+import jdk.management.resource.internal.inst.FileOutputStreamRMHooks;
+
+import java.io.*;
+
+public class Ex01_01 {
+
+    public static void main(String[] args) throws IOException{
+        //System.out.println("Test");
+        FileOutputStream fos = new FileOutputStream(FileDescriptor.out);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        byte[] bb = {'a', 'b', 'c'};
+        System.out.write(bb);
+        dos.write(bb);
+        dos.writeInt(12);
+        dos.writeFloat(14.4f);
+        dos.flush();
+
+    }
+}
