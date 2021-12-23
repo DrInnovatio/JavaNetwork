f3f40c8efd63df01b5e1f0f80b57471c17a59ba8
DrInnovatio <turing1723@gmail.com>
2021-12-23 14:35:35 +0900

    URLClassLoader.

---
+++ src/Fourth/Ex04_04.java
@@ -0,0 +1,20 @@
+package Fourth;
+
+import java.io.*;
+import java.net.*;
+import java.net.MalformedURLException;
+
+public class Ex04_04 {
+    public static void main(String[] args) {
+        File f = new File("C:/Users/user/IdeaProjects/JavaNetwork/AAA/");
+        File f1 = new File("C:/Users/user/IdeaProjects/JavaNetwork/BBB/");
+/*
+        try{
+            URL[] url = {f.toURL(), f1.toURL()};
+            URLClassLoader ucl = new URLClassLoader(url);
+            System.out.println("ucl = " + ucl.toString());
+        }catch (MalformedURLException ee){}
+
+ */
+    }
+}
